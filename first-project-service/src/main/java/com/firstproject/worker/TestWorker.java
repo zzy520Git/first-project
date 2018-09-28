8699cfccc34b3427a7543f9ba3876747898a2cc4
zhouzhongyi1 <zhouzhongyi1@jd.com>
2018-09-28 12:04:53 +0800

    worker

---
+++ first-project-service/src/main/java/com/firstproject/worker/TestWorker.java
@@ -0,0 +1,29 @@
+package com.firstproject.worker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Description：
+ * 任务worker
+ * @author zhouzhongyi1
+ * DATE： 2018/9/28 11:30
+ */
+@Service("testWorker")
+public class TestWorker {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestWorker.class) ;
+    //必须public
+    public void doWork1() {
+        LOGGER.warn("测试doWork1");
+    }
+
+    //必须public
+    public void doWork2() {
+        LOGGER.warn("测试doWork2");
+    }
+
+    public void doWork3() {
+        LOGGER.warn("task-schedule测试doWork3");
+    }
+}
